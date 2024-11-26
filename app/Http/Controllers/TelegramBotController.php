4a5a24f5ee6aec08c7e07d5297253bea9e17fbd9
malenceamat <yatikivan@gmail.com>
2024-11-27 00:01:29 +0300

    :)

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -35,7 +35,7 @@ class TelegramBotController extends Controller
     {
         Http::post($this->api_url . "sendMessage", [
             'chat_id' => $chat_id,
-            'text' => $text[1][2],
+            'text' => $text,
         ]);
     }
 }
